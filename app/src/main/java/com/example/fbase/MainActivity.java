5f9937b21c7f975278f6661686148ba17291f685
Rizwan <rizwankhan131199@gmail.com>
2020-01-21 13:07:48 +0530

    insert multiple values together

--- app/src/main/java/com/example/fbase/MainActivity.java
+++ app/src/main/java/com/example/fbase/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.fbase;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -13,6 +14,8 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -21,13 +24,16 @@ import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
     private Button button;
     private EditText editText;
     DatabaseReference myRef;
     private Button read;
     private TextView textView;
-    private ValueEventListener valueEventListener;
+    private  EditText editText2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,60 +43,78 @@ public class MainActivity extends AppCompatActivity {
          myRef = database.getReference("users");
         this.button.setOnClickListener(this::runcode);
         this.read.setOnClickListener(this::readcode);
-        valueEventListener=new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String data=dataSnapshot.getValue(String.class);
-                textView.setText(data);
-                Log.d("repeated",data);
-            }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        };
-        myRef.child("user1").addValueEventListener(valueEventListener);
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        myRef.child("user1").removeEventListener(valueEventListener);
-    }
+
 
     private void runcode(View view)
     {
-        String data=editText.getText().toString();
-        myRef.child("user1").setValue(data)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void aVoid) {
-                        Toast.makeText(MainActivity.this, "Data inserted ...", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.d("on failure","On Failure"+e.getMessage());
-                    }
-                });
-        Toast.makeText(this, "this is added", Toast.LENGTH_SHORT).show();
+        // insert data
+        String name=editText.getText().toString();
+        int age=Integer.parseInt(editText2.getText().toString());
+        Map<String,Object> insertValues = new HashMap<>();
+        insertValues.put("name",name);
+        insertValues.put("age",age);
+        String key=myRef.push().getKey();
+        myRef.child(key).setValue(insertValues);
+        //myRef.child(key).child("name").setValue(name);
+        //myRef.child(key).child("age").setValue(age);
+
+
+
     }
     private void readcode(View view)
     {
-        myRef.child("user1").addValueEventListener(new ValueEventListener() {
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                for(DataSnapshot dataSnapshot1:dataSnapshot.getChildren())
+//                {
+//                    Map<String,Object> data=(Map<String,Object>)dataSnapshot1.getValue();
+//
+//                    Log.d("data","Name "+data.get("name"));
+//                    Log.d("data","age "+data.get("age"));
+//                    Log.d("data","key "+dataSnapshot.getKey());
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//                Log.d("on failure"," On Failure "+databaseError.getMessage());
+//            }
+//        });
+        myRef.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String data=dataSnapshot.getValue(String.class);
-                textView.setText(data);
-                Log.d("repeated",data);
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Map<String,Object> data=(Map<String, Object>)dataSnapshot.getValue();
+                Log.d("data","onChildAdded: Name "+data.get("name") );
+                Log.d("data","onChildAdded: Age "+data.get("age") );
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                Log.d("data","OnChildChanged");
+                //Map<String,Object> data=(Map<String, Object>)dataSnapshot.getValue();
+
+                //Log.d("data","onChildChanged: Age "+data.get("age") );
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
 
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.d("on failure"," On Failure "+databaseError.getMessage());
+
             }
         });
     }
@@ -99,5 +123,6 @@ public class MainActivity extends AppCompatActivity {
         editText=findViewById(R.id.editText);
         textView=findViewById(R.id.textView);
         read=findViewById(R.id.button2);
+        editText2=findViewById(R.id.editText2);
     }
 }
